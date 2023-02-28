c11132c4c9b271917e0a8c8dc553b012dcd88f09
wajidgbpec <126550890+wajidgbpec@users.noreply.github.com>
2023-02-28 08:19:51 +0000

    Update streamlit_app.py
    Added Emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ streamlit.title("My parents New Healthy Diner")
 streamlit.header("Breakfast Menu")
 streamlit.text("Omega 3 and blueberry oatmeal")
 streamlit.text("Chole Chawal")
-                
+streamlit.text("🥣 🥗 🐔 🥑🍞")
