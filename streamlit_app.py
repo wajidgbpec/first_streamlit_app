f4ff5b4804d9bba913fb9e2a6e22b77f5ac201da
wajidgbpec <126550890+wajidgbpec@users.noreply.github.com>
2023-02-28 08:51:50 +0000

    Update streamlit_app.py
    Reading CSV file

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,8 @@ streamlit.header("Breakfast Menu")
 streamlit.text("Omega 3 and blueberry oatmeal")
 streamlit.text("Chole Chawal")
 streamlit.text("🥣 🥗 🐔 🥑🍞")
+
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
