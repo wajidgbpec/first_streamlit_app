44b7c346dc21fffa5ccaa3c867f3848d5ff03783
wajidgbpec <126550890+wajidgbpec@users.noreply.github.com>
2023-02-28 08:00:40 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
 import streamlit
 streamlit.title("My parents New Healthy Diner")
-streamlit.text("Breakfast Menu")
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 and blueberry oatmeal")
+                
