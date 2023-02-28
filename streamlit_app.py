87a67a62d06e0278eb87b80c8f3405f4fda9eebb
wajidgbpec <126550890+wajidgbpec@users.noreply.github.com>
2023-02-28 08:03:27 +0000

    Update streamlit_app.py
    Added breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,5 @@ import streamlit
 streamlit.title("My parents New Healthy Diner")
 streamlit.header("Breakfast Menu")
 streamlit.text("Omega 3 and blueberry oatmeal")
+streamlit.text("Chole Chawal")
                 
